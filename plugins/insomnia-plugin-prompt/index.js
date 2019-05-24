8704c559e5437d974fda0cc682a234bc767d81f9
Thejas Gowda <thejasgowda.n@gmail.com>
2019-05-24 10:45:25 +0530

    Revert "Only store prompt value when actually sending (Fixes #1494)"
    This reverts commit 4b218c8f109fa7fd514baa5e1bb67f0e4f7b69f2.

--- plugins/insomnia-plugin-prompt/index.js
+++ plugins/insomnia-plugin-prompt/index.js
@@ -79,11 +79,6 @@ module.exports.templateTags = [
         return cachedValue;
       }
 
-      // Only prompt when we're actually sending
-      if (context.renderPurpose !== 'send') {
-        return cachedValue || '';
-      }
-
       const value = await context.app.prompt(title || 'Enter Value', {
         label,
         defaultValue,
